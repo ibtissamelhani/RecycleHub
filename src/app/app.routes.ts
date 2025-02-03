638f9016323b4b29cf560c84c4394c8b59d32b23
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-02-03 22:29:28 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,21 @@
 import { Routes } from '@angular/router';
 import {LandingComponent} from "./pages/landing/landing.component";
+import {AuthTemplateComponent} from "./shared/auth-template/auth-template.component";
+import {RegisterComponent} from "./authentication/register/register.component";
+import {LoginComponent} from "./authentication/login/login.component";
 
 export const routes: Routes = [
-  { path:'', component:LandingComponent}
+  { path:'', component:LandingComponent},
+  { path: 'authentication', component: AuthTemplateComponent,
+    children: [
+      {
+        path: "register",
+        component: RegisterComponent,
+      },
+      {
+        path: "login",
+        component: LoginComponent,
+      }
+    ],
+  },
 ];
---
+++ src/app/shared/auth-template/auth-template.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-auth-template',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './auth-template.component.html',
+  styles: ``
+})
+export class AuthTemplateComponent {
+
+}
